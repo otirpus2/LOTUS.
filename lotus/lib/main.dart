a3aee86ec098d84c1dde601d90183f4f8e091f7a
yadavparth2007 <yadavparth2007@gmail.com>
2026-05-10 13:47:54 +0530

    SUPA

--- lotus/lib/main.dart
+++ lotus/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'screens/login_page.dart';
 import 'screens/home.dart';
@@ -8,6 +9,11 @@ import 'screens/home.dart';
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await Supabase.initialize(
+    url: 'https://ovpvbuzbazhciqacjptt.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92cHZidXpiYXpoY2lxYWNqcHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTc2MDgsImV4cCI6MjA5Mzk3MzYwOH0.jhf_a7r2FhwvJLnIgwACOxLMziHXWazj2iSxgXLZyXs',
+  );
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
--- lotus/lib/screens/login_page.dart
+++ lotus/lib/screens/login_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'signup.dart';
 import 'home.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -218,38 +218,33 @@ class _LoginPageState extends State<LoginPage> {
                         String email = emailController.text;
                         String password = passwordController.text;
 
-                        if (
-                        email == "admin" &&
-                            password == "1234"
-                        ) {
-
-                          final prefs =
-                          await SharedPreferences.getInstance();
-
-
-                          await prefs.setBool(
-                            'isLoggedIn',
-                            true,
+                        try {
+                          final AuthResponse res = await Supabase.instance.client.auth.signInWithPassword(
+                            email: email,
+                            password: password,
                           );
 
-                          Navigator.pushReplacement(
-                            context,
+                          if (res.user != null) {
+                            final prefs = await SharedPreferences.getInstance();
+                            await prefs.setBool('isLoggedIn', true);
 
-                            MaterialPageRoute(
-                              builder: (context) => const HomePage(),
-                            ),
-                          );
-
-                        } else {
-
-                          ScaffoldMessenger.of(context).showSnackBar(
-
-                            const SnackBar(
-                              content: Text(
-                                "Invalid Email or Password",
+                            if (context.mounted) {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const HomePage(),
+                                ),
+                              );
+                            }
+                          }
+                        } catch (e) {
+                          if (context.mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Invalid Email or Password"),
                               ),
-                            ),
-                          );
+                            );
+                          }
                         }
                       },
 
